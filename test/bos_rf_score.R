a80e685c32820b3579e90ad94cfde9d49329fe82
simals89 <47834679+simals89@users.noreply.github.com>
2021-05-13 11:54:53 +0200

    Create bos_rf_score.R

---
+++ test/bos_rf_score.R
@@ -0,0 +1,12 @@
+# save as bos_rf_score.R
+
+bos_rf <- readRDS("bos_rf.rds")
+library(randomForest)
+
+#* @param df data frame of variables
+#* @post /score
+function(req, df)
+{
+    df <- as.data.frame(df)
+    predict(bos_rf, df)
+}
