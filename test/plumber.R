1d76ed0675c90a59a4539edd363905e670ac5798
simals89 <47834679+simals89@users.noreply.github.com>
2021-05-13 13:14:24 +0200

    Update plumber.R

--- test/plumber.R
+++ test/plumber.R
@@ -1,11 +1,31 @@
-#* @get /riegel
-function(time, exponent = 1.06) {
-	# API inputs are character.
-	time = as.numeric(time)
-	exponent = as.numeric(exponent)
-	# Fixed distances.
-	distance = 5
-	goal = 42.2
-	# Calculate time for goal distance.
-	time * (goal / distance) ** exponent
+#' @apiTitle My R Service
+#' @apiDescription This service runs R scripts on Google Cloud Run.
+# EXAMPLE 3
+#* iris datatable
+#* @get /iris
+#* @serializer htmlwidget
+function(){
+  
+  DT::datatable(iris)
+}
+# EXAMPLE 2
+#* Random Number from Uniform Distribution
+#* @param min Lower limit of the distribution.
+#* @param max Upper limit of the distribution.
+#* @get /runif
+#* @serializer html
+function(min = 0, max = 1){
+  
+  x <- runif(n = 1, 
+             min = as.numeric(min), 
+             max = as.numeric(max))
+  
+  paste0('<h3>', x, '</h3>')
+}
+# EXAMPLE 1
+#* Confirmation Message
+#* @get /testing
+#* @serializer text
+function(msg=""){
+  "My R Service Deployed!"
 }
