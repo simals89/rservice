9d8ba0a06ee58a88ab9b5a031d4dce7c221cb4da
simals89 <47834679+simals89@users.noreply.github.com>
2021-05-13 15:47:43 +0200

    Update plumber.R

--- test/plumber.R
+++ test/plumber.R
@@ -12,7 +12,7 @@ function(min = 0, max = 1){
              min = as.numeric(min), 
              max = as.numeric(max))
   
-  paste0('<h3>', x, '</h3>')
+  return(x)
 }
 # EXAMPLE 1
 #* Confirmation Message
