414057a40646ac84dc2d5611c4aa70b8ab5e836f
simals89 <47834679+simals89@users.noreply.github.com>
2021-05-13 14:20:54 +0200

    Update plumber.R

--- test/plumber.R
+++ test/plumber.R
@@ -5,16 +5,9 @@ if(Sys.getenv('PORT') == '') Sys.setenv(PORT = 8000)
 #* Return the product of 2 matrices
 #* @param var1 An array of values representing the first matrix.
 #* @param var2 An array of values representing the second matrix.
-#* @post /myCustomFunction/
+#* @post /myCustomFunction
 function(var1, var2){
-  
-  err <- tryCatch({
-    
-    return(data.matrix(var1) %*% data.matrix(var2))
-    
-  }, error = function(e) e)
-  
-  return(paste0('Error: ', err$message))
+  return(var1 + var2) 
 }
 #* Confirmation Message
 #* @get /
