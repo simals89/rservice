e80007162d9fef46f5dc9915bb8dc81deadedcfa
simals89 <47834679+simals89@users.noreply.github.com>
2021-05-13 14:08:20 +0200

    Update plumber.R

--- test/plumber.R
+++ test/plumber.R
@@ -6,11 +6,11 @@
 #* @param max Upper limit of the distribution.
 #* @get /runif
 #* @serializer html
-function(var1 = 0, var2 = 1){
+function(min = 0, max = 1){
   
   x <- runif(n = 1, 
-             min = as.numeric(var1), 
-             max = as.numeric(var2))
+             min = as.numeric(min), 
+             max = as.numeric(max))
   
   paste0('<h3>', x, '</h3>')
 }
